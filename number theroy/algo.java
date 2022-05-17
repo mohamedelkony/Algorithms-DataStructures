433638563ee2be93a91c532fefe744435bba83bd
mohamedelkony <konykony22@gmail.com>
2022-05-17 16:21:27 +0200

    Create algo.java

---
+++ number theroy/algo.java
@@ -0,0 +1,61 @@
+final class Algo {
+    private Algo() {
+    }
+
+    // O(log(n))
+    public static long gcd(long a, long b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+
+    // O(log(sqrt(n))
+    public static TreeSet<Integer> getDivisors(int n) {
+        var st = new TreeSet<Integer>();
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0)
+                st.add(i);
+            if (n % (n / i) == 0)
+                st.add(n / i);
+        }
+        return st;
+    }
+
+    // O(n(log(log(N)))
+    public static ArrayList<Integer> primesTill_N(int n) {
+        var ans = new ArrayList<Integer>();
+        var prime = new ArrayList<Integer>(n + 1);
+        for (int i = 0; i < n + 1; i++)
+            prime.add(1);
+
+        for (int p = 2; p * p <= n; p++) {
+            if (prime.get(p) == 1) {
+                for (int i = p * p; i <= n; i += p)
+                    prime.set(i, 0);
+            }
+        }
+
+        for (int p = 2; p <= n; p++)
+            if (prime.get(p) == 1)
+                ans.add(p);
+        return ans;
+    }
+
+    // O(sqrt(N))
+    public static TreeMap<Integer, Integer> getPrimeFactors(int n) {
+        var ans = new TreeMap<Integer, Integer>();
+        var primes = primesTill_N((int) (Math.sqrt(n) + 1));
+        for (int p : primes) {
+            while (n % p == 0) {
+                ans.putIfAbsent(p, 0);
+                ans.put(p, ans.get(p) + 1);
+                n /= p;
+            }
+        }
+        if (n > 2) {
+            ans.putIfAbsent(n, 0);
+            ans.put(n, ans.get(n) + 1);
+        }
+        return ans;
+    }
+}
